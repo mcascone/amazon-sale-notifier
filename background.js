511f515335dd2107af827b52708fccc889dedf33
Max Cascone <mcascone@consilio.com>
2019-05-19 15:56:50 -0500

    i get the api data, but not sure how to push to UI

--- background.js
+++ background.js
@@ -3,6 +3,10 @@
 // found in the LICENSE file.
 
 // This is purely to figure out how to place an API call to a simple test endpoint.
-fetch('https://jsonplaceholder.typicode.com/todos/2')
+fetch('https://jsonplaceholder.typicode.com/todos/3')
   .then(response => response.json())
   .then(json => console.log(json))
+
+
+  // now how do i show it in the popup?
+
